ca55d64c1df23c96a1305297903ecbbf44175cfc
SMARAK PRADHAN <samarakpradhan@gmail.com>
2025-10-16 17:56:27 +0530

    eslint fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,  
+        ...globals.jest,   
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+];
